f2a95c30ab21a52db71229f17af4aa070cc248c7
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-16 14:26:19 -0600

    initial files.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./models');
+const db = require(path.join(__dirname, './models'));
 
 const PORT = process.env.PORT || 8080;
 const app = express();
