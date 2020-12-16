c4c6ff5fac113598a032e48ed5a85259cf4add92
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-16 14:24:12 -0600

    initial files.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const db = require('./models');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//parse parameters
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+db.sequelize.sync().then(() => {
+    //bind server.
+    app.listen(PORT, () => {
+        console.log("Server is now listening on port: " + PORT);
+    });
+});
