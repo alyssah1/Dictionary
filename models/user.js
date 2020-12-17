378208e3d25e5c88ad315aa8cb60cfa7ce0efc07
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-17 09:14:52 -0600

    new user restrictions.

--- models/user.js
+++ models/user.js
@@ -1,32 +1,42 @@
-// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-// noinspection SpellCheckingInspection
 const bcrypt = require("bcryptjs");
 
-// Creating our User model
 module.exports = function(sequelize, DataTypes) {
+    /**
+     * username must be alphanumerical, and must be at least 6 letters long, not empty.
+     * password must be at least 8 letters long
+     */
     const User = sequelize.define("User", {
-        // The email cannot be null, and must be a proper email before creation
         username: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
             validate: {
-                len: [6,160]
+                len: [6,160],
+                notEmpty: true,
+                isAlphanumeric: true
             }
         },
         // The password cannot be null
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [8,160]
+            }
         }
     });
-    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+
+    /**
+     * check unhashed password against user's hashed password
+     */
     User.prototype.validPassword = function(password) {
         // noinspection JSCheckFunctionSignatures
         return bcrypt.compareSync(password, this.password);
     };
-    // Hooks are automatic methods that run during various phases of the User Model lifecycle
-    // In this case, before a User is created, we will automatically hash their password
+
+    /**
+     * hash the passwords before saving it into the database
+     */
     User.addHook("beforeCreate", function(user) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
     });
