ef821d6bde16159ea5999364238879f61cb9ab91
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-17 09:08:06 -0600

    authentication routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require(path.join(__dirname, './models'));
+var exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -9,6 +10,13 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//config express handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(require('./routes/authentication'));
+
+//sync models before active server
 db.sequelize.sync().then(() => {
     //bind server.
     app.listen(PORT, () => {
