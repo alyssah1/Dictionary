e0b498a315899bc09f393ed3c8b9dff0aa7e99e0
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-17 10:24:35 -0600

    new user restrictions.

--- routes/authentication.js
+++ routes/authentication.js
@@ -6,7 +6,11 @@ const router = require('express').Router();
  * check if user is logged in unless they are visiting the login page or the sign up page.
  */
 router.use('/', (req, res, next) => {
-    if(req.user || req.url.toLowerCase() === "/login" || req.url.toLowerCase() === "/signup") {
+    if(req.user ||
+        req.url.toLowerCase() === "/login" ||
+        req.url.toLowerCase() === "/signup" ||
+        req.url.toLowerCase() === "/api/login" ||
+        req.url.toLowerCase() === "/api/signup") {
         return next();
     } else {
         res.redirect('/login');
@@ -21,5 +25,21 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.post('/api/login', passport.authenticate("local"), (req, res) => {
+    // noinspection JSUnresolvedVariable
+    res.json(req.user);
+});
+
+router.post('/api/signup', (req, res) => {
+    // noinspection JSUnresolvedVariable
+    db.User.create({
+        username: req.body.username,
+        password: req.body.password
+    }).then(() => {
+        res.redirect(307, "/api/login");
+    }).catch(err => {
+        res.status(401).json(err);
+    });
+});
 
 module.exports = router;
