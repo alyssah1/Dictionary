d95f0daddf44497f16a0c24412ee88e111cd1817
Fanghan Hu <Fanghan.Hu@gmail.com>
2020-12-18 18:49:01 -0600

    code quality

--- models/word.js
+++ models/word.js
@@ -1,20 +1,20 @@
-module.exports = function(sequelize, DataTypes) {
-const Word = sequelize.define("Word", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1,140]
+module.exports = function (sequelize, DataTypes) {
+    const Word = sequelize.define("Word", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140]
+            }
         }
-    }
-});
-
-Word.associate = function(models) {
-
-    Word.belongsToMany(models.User, {through: 'UserWords'});
-    Word.hasMany(models.Definition, {
-        onDelete: "cascade"
     });
-};
+
+    Word.associate = function (models) {
+
+        Word.belongsToMany(models.User, {through: 'UserWords'});
+        Word.hasMany(models.Definition, {
+            onDelete: "cascade"
+        });
+    };
     return Word;
 };
